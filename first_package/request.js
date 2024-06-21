11fb9e5f1404651e56de461260fce1c30162b35a
YourNewUsername <megha.gupta@avisoft.io>
2024-06-21 18:27:09 +0530

    request

---
+++ first_package/request.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+axios.get("https://www.google.com")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .then((err)=>{
+    console.log("All done");
+  });
